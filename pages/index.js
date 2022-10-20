bf953a1b847a90ce1d3de0ceecdb4c302cf87767
Aadit-Ambadkar <aadit.ambadkar@gmail.com>
2022-10-19 22:06:49 -0700

    Firebase Auth

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-export default function Home() {
+import { getSession } from "next-auth/react";
+import { useEffect, useState, } from 'react';
+import { signIn } from "next-auth/react"
+
+export default function Home(props) {
   return (
-    <main>hello world!</main>
+    <>
+      <main>hello world!</main>
+      <a onClick={signIn} className='cursor-pointer'>
+        <span>Sign In</span>
+      </a>
+    </>
   );
 }
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
+};
